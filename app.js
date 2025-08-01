093ca14095f3e229fe1b2f6efcbbbc90101bef15
brunkeralf <brunkeralf@gmail.com>
2025-08-01 12:51:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import SecondScreen from './SecondScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [page, setPage] = React.useState('home');
-
-  if (page === 'second') {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Du är på sida 2</Text>
-        <Button title="Tillbaka" onPress={() => setPage('home')} />
-      </View>
-    );
-  }
-
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Startsida</Text>
-      <Button title="Gå till sida 2" onPress={() => setPage('second')} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
